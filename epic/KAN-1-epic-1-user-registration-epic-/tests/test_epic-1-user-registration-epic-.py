9f5425e1be8be47767bf8a4f4808f375f904da67
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-09 15:00:15 +0530

    Add test file for KAN-1

---
+++ epic/KAN-1-epic-1-user-registration-epic-/tests/test_epic-1-user-registration-epic-.py
@@ -0,0 +1,5 @@
+def test_epic-1-user-registration-epic-():
+    # Given: Epic 1: User Registration Epic...
+    # When: User Registration Epic
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
