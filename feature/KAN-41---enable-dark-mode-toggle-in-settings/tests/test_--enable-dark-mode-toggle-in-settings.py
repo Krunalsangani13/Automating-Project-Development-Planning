a7d4c1d50803088acdd581c5cf04de7beecba2a0
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:43 +0530

    Add test file for KAN-41

---
+++ feature/KAN-41---enable-dark-mode-toggle-in-settings/tests/test_--enable-dark-mode-toggle-in-settings.py
@@ -0,0 +1,5 @@
+def test_--enable-dark-mode-toggle-in-settings():
+    # Given: - Enable dark mode toggle in settings
+    # When: - Enable dark mode toggle in settings
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
