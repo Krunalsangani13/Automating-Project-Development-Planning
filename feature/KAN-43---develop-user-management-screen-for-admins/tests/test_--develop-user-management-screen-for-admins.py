99ec0fa7f0828dc207c0443afc4ce260c7926356
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:36 +0530

    Add test file for KAN-43

---
+++ feature/KAN-43---develop-user-management-screen-for-admins/tests/test_--develop-user-management-screen-for-admins.py
@@ -0,0 +1,5 @@
+def test_--develop-user-management-screen-for-admins():
+    # Given: - Develop user management screen for admins
+    # When: - Develop user management screen for admins
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
