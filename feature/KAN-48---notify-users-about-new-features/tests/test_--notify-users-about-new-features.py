37bb4aa9a0755f7640d713d31341c5848551aa07
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:15 +0530

    Add test file for KAN-48

---
+++ feature/KAN-48---notify-users-about-new-features/tests/test_--notify-users-about-new-features.py
@@ -0,0 +1,5 @@
+def test_--notify-users-about-new-features():
+    # Given: - Notify users about new features
+    # When: - Notify users about new features
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
