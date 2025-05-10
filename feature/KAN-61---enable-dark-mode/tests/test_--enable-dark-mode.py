4830d8334aff9927526922f043f991e091f1b2ac
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 23:12:32 +0530

    Add test file for KAN-61

---
+++ feature/KAN-61---enable-dark-mode/tests/test_--enable-dark-mode.py
@@ -0,0 +1,5 @@
+def test_--enable-dark-mode():
+    # Given: - Enable dark mode
+    # When: - Enable dark mode
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
