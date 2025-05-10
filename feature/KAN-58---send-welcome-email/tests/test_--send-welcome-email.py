079d5877e382d9ae9f6449cd9778502d24fb35cb
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 23:12:49 +0530

    Add test file for KAN-58

---
+++ feature/KAN-58---send-welcome-email/tests/test_--send-welcome-email.py
@@ -0,0 +1,5 @@
+def test_--send-welcome-email():
+    # Given: - Send welcome email
+    # When: - Send welcome email
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
