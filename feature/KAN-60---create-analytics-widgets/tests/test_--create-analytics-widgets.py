01cddcdceb9a309a356006e33a4990146be3bce8
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 23:12:37 +0530

    Add test file for KAN-60

---
+++ feature/KAN-60---create-analytics-widgets/tests/test_--create-analytics-widgets.py
@@ -0,0 +1,5 @@
+def test_--create-analytics-widgets():
+    # Given: - Create analytics widgets
+    # When: - Create analytics widgets
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
