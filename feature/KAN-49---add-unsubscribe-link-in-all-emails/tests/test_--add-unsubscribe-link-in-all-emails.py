60cba4282e2dda480ac3957aeaf392d08fc94017
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:11 +0530

    Add test file for KAN-49

---
+++ feature/KAN-49---add-unsubscribe-link-in-all-emails/tests/test_--add-unsubscribe-link-in-all-emails.py
@@ -0,0 +1,5 @@
+def test_--add-unsubscribe-link-in-all-emails():
+    # Given: - Add unsubscribe link in all emails
+    # When: - Add unsubscribe link in all emails
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
