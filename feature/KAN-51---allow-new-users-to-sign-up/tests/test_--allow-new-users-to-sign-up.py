9214659676bd41e749c0bf5865d84d75bd35b6c0
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 23:04:17 +0530

    Add test file for KAN-51

---
+++ feature/KAN-51---allow-new-users-to-sign-up/tests/test_--allow-new-users-to-sign-up.py
@@ -0,0 +1,5 @@
+def test_--allow-new-users-to-sign-up():
+    # Given: - Allow new users to sign up
+    # When: - Allow new users to sign up
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
