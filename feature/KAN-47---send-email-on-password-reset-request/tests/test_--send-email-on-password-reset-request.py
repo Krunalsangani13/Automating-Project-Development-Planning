ec0ae755ce95468a42bef8e6fdb44ec7b17920b7
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:20 +0530

    Add test file for KAN-47

---
+++ feature/KAN-47---send-email-on-password-reset-request/tests/test_--send-email-on-password-reset-request.py
@@ -0,0 +1,5 @@
+def test_--send-email-on-password-reset-request():
+    # Given: - Send email on password reset request
+    # When: - Send email on password reset request
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
