fc15ad1b64e93da396e5719ff52d14c39fc0a309
Krunalsangani13 <khsangani3344@gmail.com>
2025-05-10 18:39:32 +0530

    Add test file for KAN-44

---
+++ feature/KAN-44---allow-role-assignment-admin-editor-viewer-/tests/test_--allow-role-assignment-admin-editor-viewer-.py
@@ -0,0 +1,5 @@
+def test_--allow-role-assignment-admin-editor-viewer-():
+    # Given: - Allow role assignment (admin, editor, viewer)
+    # When: - Allow role assignment (admin, editor, viewer)
+    # Then: Add your test logic here
+    assert True  # Replace with actual test logic
